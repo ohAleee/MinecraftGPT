62eb8abf71cd30a0d3851ad8d39b9a3906c3fe2d
ohAlee <alebartoh@gmail.com>
2023-04-23 00:57:00 +0200

    Fixes. Version 1.2.6

--- src/main/java/it/ohalee/minecraftgpt/OpenAI.java
+++ src/main/java/it/ohalee/minecraftgpt/OpenAI.java
@@ -1,6 +1,7 @@
 package it.ohalee.minecraftgpt;
 
 import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 import org.bukkit.configuration.ConfigurationSection;
 import retrofit2.HttpException;
@@ -20,17 +21,40 @@ public class OpenAI {
     public static CompletableFuture<String> getResponse(ConfigurationSection section, StringBuilder cached, String message) {
         cached.append("\nHuman:").append(message).append("\nAI:");
 
-        return CompletableFuture.supplyAsync(() -> service.createCompletion(CompletionRequest.builder()
-                        .prompt(cached.toString())
-                        .model(section.getString("model"))
-                        .temperature(section.getDouble("temperature"))
-                        .maxTokens(section.getInt("max-tokens"))
-                        .topP(section.getDouble("top-p"))
-                        .frequencyPenalty(section.getDouble("frequency-penalty"))
-                        .presencePenalty(section.getDouble("presence-penalty"))
-                        .stop(Arrays.asList("Human:", "AI:"))
-                        .build())
-                .getChoices().get(0).getText()).exceptionally(throwable -> {
+        return CompletableFuture.supplyAsync(() -> {
+            String model = section.getString("model", "text-davinci-003");
+            int maxTokens = section.getInt("max-tokens");
+            double frequencyPenalty = section.getDouble("frequency-penalty");
+            double presencePenalty = section.getDouble("presence-penalty");
+            double topP = section.getDouble("top-p");
+            double temperature = section.getDouble("temperature");
+
+            if (model.startsWith("gpt-4") || model.startsWith("gpt-3.5")) {
+                return service.createChatCompletion(ChatCompletionRequest.builder()
+                                .model(model)
+                                .temperature(temperature)
+                                .maxTokens(maxTokens)
+                                .topP(topP)
+                                .frequencyPenalty(frequencyPenalty)
+                                .presencePenalty(presencePenalty)
+                                .stop(Arrays.asList("Human:", "AI:"))
+                                .build())
+                        .getChoices().get(0).getMessage().getContent();
+            }
+
+            return service.createCompletion(CompletionRequest.builder()
+                            .prompt(cached.toString())
+                            .model(model)
+                            .temperature(temperature)
+                            .maxTokens(maxTokens)
+                            .topP(topP)
+                            .frequencyPenalty(frequencyPenalty)
+                            .presencePenalty(presencePenalty)
+                            .stop(Arrays.asList("Human:", "AI:"))
+                            .build())
+                    .getChoices().get(0).getText();
+
+        }).exceptionally(throwable -> {
             if (throwable.getCause() instanceof HttpException e) {
                 String reason = switch (e.response().code()) {
                     case 401 -> "Invalid API key! Please check your configuration.";
